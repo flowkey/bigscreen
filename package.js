6bc662606e301173b97b78eddd2cf3a13323fbb8
Ahmed Hassan <a.hassan@buismed.de>
2015-10-07 15:55:14 +0200

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'flowkey:bigscreen',
   summary: 'A simple library for using the JavaScript Fullscreen API',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/flowkey/bigscreen.git'
 });
 
